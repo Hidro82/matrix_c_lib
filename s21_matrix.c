d279edb57289bd52d1af06d08b4fa2a78c4aeb19
blackhai <blackhai@student.21-school.ru>
2022-08-01 17:37:47 +0300

    Need complements, reverse, tests and makefile

--- s21_matrix.c
+++ s21_matrix.c
@@ -98,7 +98,7 @@ int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
     int errCode = 0;
 
-    if ((A->columns != B->columns) || (A->rows != B->rows)) {
+    if ((A->columns != B->rows) || (A->rows != B->columns)) {
         errCode = 1;
     } else {
         s21_create_matrix(A->rows, A->columns, result);
@@ -127,4 +127,44 @@ int s21_transpose(matrix_t *A, matrix_t *result) {
     return errCode;
 }
 
-int s21_calc_complements(matrix_t *A, matrix_t *result);
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+    
+}
+
+int s21_determinant(matrix_t *A, double *result) {
+    int errCode = 0;
+    int coor_1 = 0;
+    int coor_2 = 0;
+    double diag = 1;
+    double deter = 0;
+    
+    if ((A->columns!= A->rows) || (A->rows <= 0) || (A->columns <= 0)) {
+        errCode = 1;
+    } else {
+        for (int i = 0; i < A->rows; i++) {
+            for (int j = 0; j < A->columns; j++) {
+                coor_1 = j;
+                coor_2 = j + i;
+                if (coor_2 >= A->rows)
+                    coor_2 -= A->rows;
+                diag *= A->matrix[coor_1][coor_2];
+            }
+            deter += diag;
+            diag = 1;
+        }
+        diag = -1;
+        for (int i = 2; i >= 0; i--) {
+            for (int j = 0; j < A->columns; j++) {
+                coor_1 = j;
+                coor_2 = i - j;
+                if (coor_2 < 0)
+                    coor_2 += A->rows;
+                diag *= A->matrix[coor_1][coor_2];
+            }
+            deter += diag;
+            diag = -1;
+        }
+        *result = deter;
+    }
+    return errCode;
+}
